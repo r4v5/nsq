20f8d570d475d46093a176ff83acc0dc0e28d364
Mason Donahue <mason@sproutsocial.com>
2018-08-01 15:15:32 -0500

    Make it compile, release custom 0.3.8-sprout version

--- internal/test/assertions.go
+++ internal/test/assertions.go
@@ -37,7 +37,7 @@ func Nil(t *testing.T, object interface{}) {
 func NotNil(t *testing.T, object interface{}) {
 	if isNil(object) {
 		_, file, line, _ := runtime.Caller(1)
-		t.Logf("\033[31m%s:%d:\n\n\tExpected value not to be <nil>\033[39m\n\n",
+		t.Logf("\033[31m%s:%d:\n\n\tExpected value not to be <nil>: %s\033[39m\n\n",
 			filepath.Base(file), line, object)
 		t.FailNow()
 	}
